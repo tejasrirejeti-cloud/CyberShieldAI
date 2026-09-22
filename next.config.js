4c29f3c3fc56dea920f88cc418804859a86a1ecb
Tejasri Rejeti <tejasrirejeti@gmail.com>
2026-09-22 23:06:24 +0530

    fix: configure Next.js for Vercel root deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   images: {
     unoptimized: true,
   },
-  basePath: "/CyberShieldAI",
-  assetPrefix: "/CyberShieldAI",
   trailingSlash: true,
 };
 
